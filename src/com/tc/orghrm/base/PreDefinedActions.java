e6a18c28819d1d7d4f5672f96799dac3a3f35f23
Titiksha Goel <titiksha goel>
2022-11-10 09:15:48 +0530

    important

--- src/com/tc/orghrm/base/PreDefinedActions.java
+++ src/com/tc/orghrm/base/PreDefinedActions.java
@@ -17,13 +17,7 @@ public class PreDefinedActions {
 	
 	public static void closeBrowser() {
 		driver.close();
-<<<<<<< Updated upstream
-		System.out.println("-----------------");
-=======
-		System.out.println("--------------------");
-		System.out.println("--------------------");
-		System.out.println("--------------------");
-		System.out.println("--------------------");
->>>>>>> Stashed changes
+		
+
 	}
 }
